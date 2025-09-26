e89a3391d3ee02d8ac448d156ed3f5020c6522b3
Artjoms Andrejevs <artjomsandrejevs@Artjomss-MacBook-Air.local>
2025-09-26 13:06:52 +0300

    adding templates

---
+++ templates/plusMinusBlock.js
@@ -0,0 +1,88 @@
+<div class="plus-minus-block purple-highlight" id="undefined">
+            <h2
+              style="margin-bottom: 2rem"
+              data-id="020dfec6-469a-42bb-947c-74ac345d7d11"
+            >
+              <span id="pros-and-cons"></span>
+              Pros of Howl Casino
+            </h2>
+            <div class="plus-minus-content">
+              <div class="plus-minus-list">
+                <div class="plus-minus-item">
+                  <div class="plus-minus-item-img">
+                    <img
+                    loading="lazy"
+                      data-image="plusIcon"
+                      src="https://img.icons8.com/?size=100&id=pmI82Oq5PUQF&format=png&color=40C057"
+                      width="2048"
+                      height="2048"
+                      alt="Icon"
+                    />
+                  </div>
+                  <p data-id="6dc5357c-10d0-4510-9082-23b952a06517">
+                    Wide selection of games
+                  </p>
+                </div>
+                <div class="plus-minus-item">
+                  <div class="plus-minus-item-img">
+                    <img
+                    loading="lazy"
+                      data-image="plusIcon"
+                      src="https://img.icons8.com/?size=100&id=pmI82Oq5PUQF&format=png&color=40C057"
+                      width="2048"
+                      height="2048"
+                      alt="Icon"
+                    />
+                  </div>
+                  <p data-id="6dc5357c-10d0-4510-9082-23b952a06517">
+                    Fast and secure payments
+                  </p>
+                </div>
+                <div class="plus-minus-item">
+                  <div class="plus-minus-item-img">
+                    <img
+                    loading="lazy"
+                      data-image="plusIcon"
+                      src="https://img.icons8.com/?size=100&id=pmI82Oq5PUQF&format=png&color=40C057"
+                      width="2048"
+                      height="2048"
+                      alt="Icon"
+                    />
+                  </div>
+                  <p data-id="6dc5357c-10d0-4510-9082-23b952a06517">
+                    Exclusive VIP rewards
+                  </p>
+                </div>
+                <div class="plus-minus-item">
+                  <div class="plus-minus-item-img">
+                    <img
+                    loading="lazy"
+                      data-image="plusIcon"
+                      src="https://img.icons8.com/?size=100&id=pmI82Oq5PUQF&format=png&color=40C057"
+                      width="2048"
+                      height="2048"
+                      alt="Icon"
+                    />
+                  </div>
+                  <p data-id="6dc5357c-10d0-4510-9082-23b952a06517">
+                    Exciting live dealer games
+                  </p>
+                </div>
+                <div class="plus-minus-item">
+                  <div class="plus-minus-item-img">
+                    <img
+                    loading="lazy"
+                      data-image="plusIcon"
+                      src="https://img.icons8.com/?size=100&id=pmI82Oq5PUQF&format=png&color=40C057"
+                      width="2048"
+                      height="2048"
+                      alt="Icon"
+                    />
+                  </div>
+                  <p data-id="6dc5357c-10d0-4510-9082-23b952a06517">
+                    Mobile-friendly platform
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
