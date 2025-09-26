02f38c5f1f64f94ad874ded50a5afbbb60fd7647
Artjoms Andrejevs <artjomsandrejevs@Artjomss-MacBook-Air.local>
2025-09-26 12:47:39 +0300

    Sequelize init

---
+++ migrations/20250926094223-add-casino-template.js
@@ -0,0 +1,18 @@
+'use strict';
+ 
+module.exports = {
+ async up (queryInterface, Sequelize) {
+   return queryInterface.bulkInsert('template_list', [
+     {
+       id: '8941e323-0a94-4a61-93bf-84333c7cddf9',
+       name: 'Second Template'
+     }
+   ], {});
+ },
+ 
+ async down (queryInterface, Sequelize) {
+   return queryInterface.bulkDelete('template_list', {
+     id: '8941e323-0a94-4a61-93bf-84333c7cddf9'
+   }, {});
+ }
+};Ы
